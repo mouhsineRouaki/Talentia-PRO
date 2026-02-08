ee96c0b30ac0af830d268405543db2aeb87e134d
ayouberrak <ayouberrak30@gmail.com>
2026-02-08 13:30:59 +0100

    fix fillable password

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,12 +7,13 @@ use App\UserRole;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
 use App\Models\RelationShip;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasRoles;
 
     /**
      * The attributes that are mass assignable.
@@ -26,6 +27,7 @@ class User extends Authenticatable
         'role',
         'biographie' , 
         'image',
+        'password' //hnaaa
     ];
 
     /**
