36a4a5515838f1ebfbe94bfbaa0c0886411eb9ca
ayouberrak <ayouberrak30@gmail.com>
2026-02-10 16:57:54 +0100

    Update MessageSent event for improved real-time broadcasting

--- app/Events/MessageSent.php
+++ app/Events/MessageSent.php
@@ -1,25 +1,28 @@
 <?php
 
 namespace App\Events;
-
+use App\Models\Message;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class MessageSent
+class MessageSent implements ShouldBroadcastNow
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $message;
 
     /**
      * Create a new event instance.
      */
-    public function __construct()
+    public function __construct(Message $message)
     {
-        //
+        $this->message = $message;
     }
 
     /**
@@ -30,7 +33,12 @@ class MessageSent
     public function broadcastOn(): array
     {
         return [
-            new PrivateChannel('channel-name'),
+            new PrivateChannel('conversation.'.$this->message->conversation_id),
         ];
+    }
+
+    public function broadcastAs()
+    {
+        return 'message.sent';
     }
 }
