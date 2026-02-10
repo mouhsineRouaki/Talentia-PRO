d5ba90b3cdcfd735fffa4bda67e6ca17fdedc06d
ayouberrak <ayouberrak30@gmail.com>
2026-02-10 16:58:00 +0100

    Update broadcasting channels configuration

--- routes/channels.php
+++ routes/channels.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Broadcast;
+use App\Models\Conversation;
 
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
+});
+
+Broadcast::channel('conversation.{id}', function ($user, $id) {
+    $conversation = Conversation::find($id);
+    return $conversation && in_array($user->id, [
+            $conversation->user_one_id,
+            $conversation->user_two_id
+        ]);
 });
